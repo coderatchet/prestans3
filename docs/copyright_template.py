31bc3f31731fa3014975bba076c843929866c327
Jared <jared@anomaly.net.au>
2016-09-16 15:55:55 +1000

    added template for python copyrights

---
+++ docs/copyright_template.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    prestans3.package.name.file
+    ~~~~~~~~~~~~~~
+
+    A WSGI compliant REST micro-framework.
+
+    :copyright: (c) 2016 Anomaly Software
+    :license: Apache 2.0, see LICENSE for more details.
+"""
