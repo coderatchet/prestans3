8be01066918f1feb24701a1e1a161a0fb117de94
Jared <jared@anomaly.net.au>
2016-10-06 09:46:15 +1100

    added output to console for run_coverage/coveralls.py commands

--- run_coverage.py
+++ run_coverage.py
@@ -2,9 +2,14 @@
 
 import os
 
-from subprocess import call
+from subprocess import Popen, PIPE, STDOUT
 
 if __name__ == '__main__':
     if 'TRAVIS' in os.environ and 'TRAVIS_PYTHON_VERSION' in os.environ and os.environ['TRAVIS_PYTHON_VERSION'] == '3.5':
-        rc = call(['coverage', 'run', '--source=prestans3', 'python', os.path.expandvars('${toxinidir}/setup.py'), 'test'])
+        rc = Popen(['coverage', 'run', '--source=prestans3', 'python', os.path.expandvars('${toxinidir}/setup.py'), 'test'], stdout=PIPE, stderr=STDOUT)
+        while True:
+            line = rc.stdout.readline()
+            if not line:
+                break
+            print(line)
         raise SystemExit(rc)
--- run_coveralls.py
+++ run_coveralls.py
@@ -2,9 +2,14 @@
 
 import os
 
-from subprocess import call
+from subprocess import Popen, PIPE, STDOUT
 
 if __name__ == '__main__':
     if 'TRAVIS' in os.environ and 'TRAVIS_PYTHON_VERSION' in os.environ and os.environ['TRAVIS_PYTHON_VERSION'] == '3.5':
-        rc = call('coveralls')
+        rc = Popen(['coveralls'], stdout=PIPE, stderr=STDOUT)
+        while True:
+            line = rc.stdout.readline()
+            if not line:
+                break
+            print(line)
         raise SystemExit(rc)
