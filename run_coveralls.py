4a83d96bd91e4d1cf21eb08b14cee5305caf3105
Jared <jared@anomaly.net.au>
2016-10-06 11:19:14 +1100

    I'm done...

--- run_coveralls.py
+++ run_coveralls.py
@@ -2,12 +2,12 @@
 
 import os
 import sys
-from subprocess import Popen, STDOUT
+from subprocess import Popen, STDOUT, PIPE
 
 if __name__ == '__main__':
     if 'TRAVIS' in os.environ and os.environ.get('TRAVIS_PYTHON_VERSION', 'unknown-version') == '3.5':
         coverage_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.coveragerc')
-        rc = Popen(['coveralls'], stderr=STDOUT)
+        rc = Popen(['coveralls'], stdout=PIPE, stderr=STDOUT)
         while True:
             line = rc.stdout.readline()
             if not line:
