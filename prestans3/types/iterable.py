9442174019ee065eb71ce59f0f83edbdf6ef974e
Jared <jared@anomaly.net.au>
2016-10-10 08:32:09 +1100

    copyright for iterable

---
+++ prestans3/types/iterable.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    prestans.types.iterable
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    A WSGI compliant REST micro-framework.
+
+    :copyright: (c) 2016 Anomaly Software
+    :license: Apache 2.0, see LICENSE for more details.
+"""
