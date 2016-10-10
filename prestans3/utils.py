177a02ad8a890de8ae325b5569726428c3d71aac
Jared <jared@anomaly.net.au>
2016-10-10 12:17:27 +1100

    can inject class

--- prestans3/utils.py
+++ prestans3/utils.py
@@ -8,6 +8,7 @@
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
+from copy import copy
 from functools import lru_cache
 
 try:
@@ -25,14 +26,35 @@ except NameError:
         return isinstance(s, str)
 
 
-@lru_cache
-def inject_class_in_hierarchy(template_class, class_to_inject, target_base_class=object):
+def prefix_with_injected(template_class, class_to_inject, target_base_class):
+    return "Injected{}".format(template_class.__name__)
+
+
+@lru_cache(maxsize=100)
+def inject_class(template_class, class_to_inject, target_base_class=object, new_type_name_func=None):
     """
-    injects a class above a target class in the mro of the specified class
+    injects a class above a target class in the mro of the specified class. New class's default name is
+    Injected<name of template_class>
 
     :param type template_class: the class to have its hierarchy modified to create a new class
     :param type class_to_inject: the class to inject about the target_base_class
     :param type target_base_class: the class to inject the class above (i.e. type(class_to_inject, bases=target_base_class, dict=class_to_inject.__dict__))
+    :param new_type_name_func: function that determines the name of the new class to be generated
+     :type new_type_name_func: (type, type, type) -> str
     :return: the new modified type
     """
+    if new_type_name_func is None:
+        new_type_name_func = prefix_with_injected
 
+    new_type_name = new_type_name_func(template_class, class_to_inject, target_base_class)
+
+    _bases = list(copy(template_class.__bases__))
+    if target_base_class in _bases:
+        index_of_target = _bases.index(target_base_class)
+        _bases.insert(index_of_target, class_to_inject)
+        return type(new_type_name, tuple(_bases), dict(class_to_inject.__dict__))
+    else:
+        _bases = [inject_class(base, class_to_inject, target_base_class, new_type_name_func)
+                  for base in _bases if target_base_class in base.mro()] \
+                 + [base for base in _bases if target_base_class not in base.mro()]
+        return type(new_type_name, tuple(_bases), dict(template_class.__dict__))
---
+++ tests/test_utils.py
@@ -0,0 +1,48 @@
+from prestans3.utils import is_str, inject_class
+
+
+def test_is_str():
+    assert is_str("yes")
+    assert not is_str(1)
+
+
+class InjectableClass(object):
+    pass
+
+
+def test_inject_class():
+    class MyClass(object):
+        pass
+
+    new_type = inject_class(MyClass, InjectableClass)
+    assert new_type.__bases__ == (InjectableClass, object)
+
+
+def test_can_inject_class_with_more_than_one_subclass():
+    class __A(object):
+        pass
+
+    class __B(object):
+        pass
+
+    class __C(__A, __B):
+        pass
+
+    new_type = inject_class(__C, InjectableClass)
+    assert new_type.__name__ == 'Injected{}'.format(__C.__name__)
+    first_base = new_type.__bases__[0]
+    assert first_base.__name__ == 'Injected{}'.format(__A.__name__)
+    assert first_base.__bases__ == (InjectableClass, object)
+    second_base = new_type.__bases__[1]
+    assert second_base.__name__ == 'Injected{}'.format(__B.__name__)
+    assert second_base.__bases__ == (InjectableClass, object)
+
+def test_can_inject_before_custom_class():
+    class __A(object):
+        pass
+
+    class __B(object):
+        pass
+
+    class __C(__A, __B):
+        pass
