a6c3ed4011f044a2fb619ac0ae29c2188c1835c5
Jared <jared@anomaly.net.au>
2016-10-13 15:54:19 +1100

    can create integer

--- tests/types/test_integer.py
+++ tests/types/test_integer.py
@@ -2,16 +2,12 @@
 
 import pytest
 
-from prestans3.errors import ValidationException
-from prestans3.types import Integer, ImmutableType
+from prestans3.types import Integer
 
 
-# def test_can_create_integer():
-#     class HasInteger:
-#         myInt = Integer(1)
-#
-#     HasInteger()
-#
+def test_can_create_integer():
+    myInt = Integer(1)
+
 #
 # # noinspection PyTypeChecker
 # def test_can_use_prestans3_integer_with_native_int_magic_methods_int():
