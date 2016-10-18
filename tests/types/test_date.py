930d5623a4bd086242d3022fe9398ed20231b464
Jared <jared@anomaly.net.au>
2016-10-18 15:39:39 +1100

    can create date

--- tests/types/test_date.py
+++ tests/types/test_date.py
@@ -8,4 +8,8 @@
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
+from prestans3.types import Date
 
+
+def test_can_create_date():
+    Date()
