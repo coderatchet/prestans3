ca808d8608dc385f0a2ebdbb1e69ae5f62ec5c59
Jared <jared@anomaly.net.au>
2016-10-19 10:00:20 +1100

    added blank test_time.py

---
+++ tests/types/test_time.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    test.types.test_time
+    ~~~~~~~~~~~~~~~~~~~~
+
+    A WSGI compliant REST micro-framework.
+
+    :copyright: (c) 2016 Anomaly Software
+    :license: Apache 2.0, see LICENSE for more details.
+"""
