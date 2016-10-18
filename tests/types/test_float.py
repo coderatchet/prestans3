43e839d36779002ff72c1a0e7a21cca48f3da74e
Jared <jared@anomaly.net.au>
2016-10-18 14:23:48 +1100

    can initialize float

--- tests/types/test_float.py
+++ tests/types/test_float.py
@@ -8,3 +8,8 @@
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
+from prestans3.types import Float
+
+
+def test_can_create_float():
+    Float()
