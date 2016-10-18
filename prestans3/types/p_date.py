315c6a8ff69cd2403d8661c24ddba82b536afeaa
Jared <jared@anomaly.net.au>
2016-10-18 15:48:14 +1100

    can init Date

--- prestans3/types/p_date.py
+++ prestans3/types/p_date.py
@@ -8,6 +8,7 @@
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
+from datetime import date
 
 from . import ImmutableType
 
@@ -17,5 +18,12 @@ class Date(ImmutableType):
     Prestans3 Date Type.
     """
 
-    def __init__(self):
+    def __init__(self, date_or_year, month=None, day=None):
+        if month is None and day is None and isinstance(date_or_year, date):
+            self._value = date_or_year
+        elif bool(month is None) != bool(day is None):
+            raise ValueError(
+                "both month and day must be specified if calling {}(year, month, day)".format(self.__class__.__name__))
+        else:
+            self._value = date(date_or_year, month, day)
         super().__init__()
--- tests/types/test_date.py
+++ tests/types/test_date.py
@@ -8,8 +8,14 @@
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
+from datetime import date
+
 from prestans3.types import Date
 
 
 def test_can_create_date():
-    Date()
+    Date(2000, 1, 1)
+
+
+def test_can_init_date():
+    Date(date(2000, 1, 1))
