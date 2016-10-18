4f0b3fbf845247fdd248c9e962a252791a6fb5f4
Jared <jared@anomaly.net.au>
2016-10-19 09:59:34 +1100

    DateTime class can init and from_value

--- prestans3/types/p_datetime.py
+++ prestans3/types/p_datetime.py
@@ -15,6 +15,19 @@ from prestans3.types.temporal import Temporal
 
 # noinspection PyAbstractClass
 class DateTime(Temporal, datetime):
-    def __init__(self):
+    def __new__(cls, year, month=None, day=None, hour=0, minute=0, second=0, microsecond=0, tzinfo=None):
+        return datetime.__new__(cls, year, month, day, hour, minute, second, microsecond, tzinfo)
+
+    def __init__(self, year, month=None, day=None, hour=0, minute=0, second=0, microsecond=0, tzinfo=None):
+        datetime.__init__(year, month, day, hour, minute, second, microsecond, tzinfo)
         super(DateTime, self).__init__()
-    pass
+
+    @classmethod
+    def from_value(cls, value):
+        if isinstance(value, cls):
+            return value
+        elif not isinstance(value, datetime):
+            raise TypeError(
+                "{} of type {} not coercible to type {}".format(value, value.__class__.__name__, cls.__name__))
+        return DateTime(value.year, value.month, value.day, value.hour, value.minute, value.second, value.microsecond,
+                        value.tzinfo)
