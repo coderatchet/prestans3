62f7c7634ee11bd770840695f4e52b2950cb6310
Jared <jared@anomaly.net.au>
2016-10-18 14:22:54 +1100

    empty test file for float

--- tests/types/test_float.py
+++ tests/types/test_float.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-    prestans.types.float
-    ~~~~~~~~~~~~~~~~~~~~
-    
+    tests.types.test_float
+    ~~~~~~~~~~~~~~~~~~~~~~
+
     A WSGI compliant REST micro-framework.
 
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
-
-from . import Number
-
-
-class Float(Number, float):
-    pass
