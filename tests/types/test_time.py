5c0df100ac6040e9e9d027a1e6dafc5b40e7449a
Jared <jared@anomaly.net.au>
2016-10-19 13:35:44 +1100

    fixed Time class not accepting time value in from_value method

--- tests/types/test_time.py
+++ tests/types/test_time.py
@@ -10,6 +10,7 @@
 """
 from datetime import time, tzinfo
 
+import pytest
 from prestans3.types import Time
 
 
@@ -23,14 +24,20 @@ class _UTC(tzinfo):
     def utcoffset(self, dt):
         return 0
 
+
 utc = _UTC()
+
 
 def test_can_create_time():
     the_time = Time(1, 2, 3, 4, utc)
     assert isinstance(the_time, Time)
 
 
-def can_from_value_create_time():
-    the_time = Time(1, 2, 3, 4)
+def test_can_from_value_create_time():
+    the_time = Time(1, 2, 3, 4, utc)
     assert Time.from_value(the_time) is the_time
     assert Time.from_value(time(1, 2, 3, 4, utc)) == the_time
+    with pytest.raises(TypeError) as error:
+        Time.from_value("pickle")
+    assert '{} of type {} is not coercible to type {}'.format("pickle", str.__name__, Time.__name__) \
+           in str(error.value)
