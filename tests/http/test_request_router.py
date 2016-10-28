47b5a99260f66b71e5bd454a2881e51bcb44c18d
Jared <jared@anomaly.net.au>
2016-10-28 12:04:37 +1100

    added stub test_request_router file

---
+++ tests/http/test_request_router.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.http.test_request_router
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    A WSGI compliant REST micro-framework.
+
+    :copyright: (c) 2016 Anomaly Software
+    :license: Apache 2.0, see LICENSE for more details.
+"""
