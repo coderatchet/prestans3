0e80e612c9d8585e7d3d7ad3e07ac8be54ecd5b9
Jared <jared@anomaly.net.au>
2016-10-28 14:02:34 +1100

    request router redirects route

--- prestans3/http/request_router.py
+++ prestans3/http/request_router.py
@@ -8,15 +8,19 @@
     :copyright: (c) 2016 Anomaly Software
     :license: Apache 2.0, see LICENSE for more details.
 """
+import re
 
 
 class RequestRouter(object):
-
     def __init__(self, routes):
         self._routes = routes
 
     def __call__(self, environ, start_response):
-        pass
+        for expression, route in self.routes:
+            regex = re.compile(expression)
+            if regex.match(environ.get("PATH_INFO", '')):
+                return route(environ, start_response)
+
 
     @property
     def routes(self):
--- tests/http/test_request_router.py
+++ tests/http/test_request_router.py
@@ -14,11 +14,22 @@ import wsgiref.util
 from prestans3.http.request_router import RequestRouter
 
 
+def setup_test_environ(overrides=None):
+    dictionary = dict(os.environ)
+    if overrides is not None:
+        dictionary.update(overrides)
+    wsgiref.util.setup_testing_defaults(dictionary)
+    return dictionary
+
+_default_wsgi_environ = setup_test_environ()
+
+
 def test_router_is_a_callable_with_two_args():
     assert callable(RequestRouter(routes=[]))
     func = RequestRouter.__call__
     var_names = func.__code__.co_varnames
-    assert len(var_names) == 2 or (len(var_names) == 3 and (var_names[0] == 'self' or var_names[0] == 'cls'))
+    arg_count = func.__code__.co_argcount
+    assert arg_count == 2 or (arg_count == 3 and (var_names[0] == 'self' or var_names[0] == 'cls'))
 
 
 def test_router_accepts_a_list_of_routes():
@@ -32,7 +43,18 @@ def test_can_get_routes():
 
 
 def test_router_may_be_passed_environment():
-    environ = dict(os.environ)
-    wsgiref.util.setup_testing_defaults(environ)
     router = RequestRouter(routes=[])
-    router(environ, lambda _x, _y: None)
+    router(_default_wsgi_environ, lambda _x, _y: None)
+
+
+def test_router_redirects_routes():
+    here = False
+
+    # noinspection PyUnusedLocal
+    def _test(x, y):
+        nonlocal here
+        here = True
+
+    router = RequestRouter(routes=[('/', _test)])
+    router(_default_wsgi_environ, lambda _x, _y: None)
+    assert here is True
