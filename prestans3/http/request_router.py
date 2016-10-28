d178bbff3a8dc5b9f2905356e0d16ff664056b56
Jared <jared@anomaly.net.au>
2016-10-28 12:03:58 +1100

    added stub request_router file

---
+++ prestans3/http/request_router.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    prestans3.http.request_router
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    A WSGI compliant REST micro-framework.
+
+    :copyright: (c) 2016 Anomaly Software
+    :license: Apache 2.0, see LICENSE for more details.
+"""
+
